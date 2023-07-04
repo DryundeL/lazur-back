a69104ce41d27dfbbfb1ec17faec167e5ce45ea9
aroongate <74457326+jamalavdoyan467@users.noreply.github.com>
2023-07-04 23:11:25 +0300

    add prometheus create metric

---
+++ app/Services/PrometheusService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+class PrometheusService extends BaseService
+{
+
+    /*
+     * Generate metric for prometheus
+     *
+     */
+    public function generateMetric($metricName, $metricType, $value, $quantiles = [], $bucketValues = [], $countValue = 0, $sumValue = 0): string
+    {
+        $metricHelp = '# HELP ' . $metricName . "\n";
+        $metricTypeLine = '# TYPE ' . $metricName . ' ' . $metricType . "\n";
+
+        switch ($metricType) {
+            case 'gauge':
+            case 'counter':
+                $metricLine = $metricName . ' ' . $value . "\n";
+                return $metricHelp . $metricTypeLine . $metricLine . "\n";
+            case 'summary':
+                $quantileLines = '';
+
+                foreach ($quantiles as $quantile => $quantileValue) {
+                    $quantileLine = $metricName . '_quantile{' . 'quantile="' . $quantile . '"} ' . $quantileValue . "\n";
+                    $quantileLines .= $quantileLine;
+                }
+
+                return $metricHelp . $metricTypeLine . $quantileLines . "\n";
+            case 'histogram':
+                $metricLines = [];
+
+                foreach ($bucketValues as $bucket => $bucketValue) {
+                    $bucketLine = $metricName . '_bucket{le="' . $bucket . '"} ' . $bucketValue . "\n";
+                    $metricLines[] = $bucketLine;
+                }
+
+                $countLine = $metricName . '_count ' . $countValue . "\n";
+                $metricLines[] = $countLine;
+
+                $sumLine = $metricName . '_sum ' . $sumValue . "\n";
+                $metricLines[] = $sumLine;
+
+                return $metricHelp . $metricTypeLine . implode("", $metricLines) . "\n";
+            default:
+                return '';
+        }
+    }
+
+}
